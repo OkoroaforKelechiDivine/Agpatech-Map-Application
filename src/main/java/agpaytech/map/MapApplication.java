fb79eb4ea3cfa3981501baabcd9ba765e6d36e87
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-06-20 12:20:54 +0100

    added file to git

---
+++ src/main/java/agpaytech/map/MapApplication.java
@@ -0,0 +1,14 @@
+package agpaytech.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapApplication.class, args);
+        System.out.println("Finished running agpaytech map application.");
+    }
+
+}
