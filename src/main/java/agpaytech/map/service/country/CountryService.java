7751365609674fbb5395585391eedcaed10c9b3e
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-06-22 15:31:22 +0100

    Fixing test cases for ther service layer

--- src/main/java/agpaytech/map/service/country/CountryService.java
+++ src/main/java/agpaytech/map/service/country/CountryService.java
@@ -1,7 +1,10 @@
 package agpaytech.map.service.country;
 
+import agpaytech.map.exception.CountryException;
+import agpaytech.map.repository.country.StaticCountries;
 import org.springframework.stereotype.Service;
 
-@Service
-public class CountryService {
+//@Service
+public interface CountryService {
+    StaticCountries findCountryByName(String name) throws CountryException;
 }
