46883f54d6e7d4cbedb10b4b5a9ed3ea43c52672
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-06-22 17:31:13 +0100

    cleaning code error

--- src/main/java/agpaytech/map/repository/country/CountryRepository.java
+++ src/main/java/agpaytech/map/repository/country/CountryRepository.java
@@ -1,14 +1,8 @@
 package agpaytech.map.repository.country;
 
-import agpaytech.map.data.country.Country;
-import agpaytech.map.exception.CountryException;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Repository
 public interface CountryRepository {
-    List<Country> countryDatabase = new ArrayList<>();
-    StaticCountries findCountryByName(String name) throws CountryException;
 }
